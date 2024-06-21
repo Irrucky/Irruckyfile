c6fca753f7cf52fce84cc3fe521a0a7f3edb9644
Irrucky <152947891+Irrucky@users.noreply.github.com>
2024-06-21 15:34:35 +0800

    更新 bili.js

---
+++ QuantumultX/rewrite/bili.js
@@ -0,0 +1,5 @@
+
+https:\/\/api\.bilibili\.com\/pgc\/player\/api\/playurl url 302 http://api.bili.best:22333/geturl/ 
+https:\/\/api\.bilibili\.com\/pgc\/view\/app\/season url 302 http://api.bili.best:22333/season
+
+hostname= api.bilibili.com
