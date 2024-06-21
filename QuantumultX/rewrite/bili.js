169001d36a3f7c7b7fdc5b42fcced7165a81087d
Irrucky <152947891+Irrucky@users.noreply.github.com>
2024-06-21 15:52:41 +0800

    Delete QuantumultX/rewrite/bili.js

--- QuantumultX/rewrite/bili.js
+++
@@ -1,5 +0,0 @@
-#自我安慰的B站大会员
-^https:\/\/api\.bilibili\.com\/pgc\/player\/api\/playurl url 302 http://api.bili.best:22333/geturl/ 
-^https:\/\/api\.bilibili\.com\/pgc\/view\/app\/season url 302 http://api.bili.best:22333/season
-
-hostname= api.bilibili.com
